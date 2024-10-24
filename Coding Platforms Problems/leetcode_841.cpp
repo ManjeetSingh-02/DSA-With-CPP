535a12a7718b3fa8ad4c31f92dd60e1dd3a748c7
Manjeet Singh <manjeetsingh8483@gmail.com>
2024-10-24 11:13:06 +0530

    Updated

--- Coding Platforms Problems/leetcode_841.cpp
+++ Coding Platforms Problems/leetcode_841.cpp
@@ -1,26 +1,26 @@
 // Keys and Rooms
 
-// bool ans = true;
-// vector<int> status(rooms.size());
-// queue<int> q;
-// for(int i = 0; i < rooms.size(); i++)
-//     status[i] = 0;
-// status[0] = 1;
-// q.push(0);
-// while (!q.empty())
-// {
-//     int x = q.front();
-//     q.pop();
-//     for (auto el : rooms[x])
-//     {
-//         if (status[el] == 1)
-//             continue;
-//         status[el] = 1;
-//         q.push(el);
-//     }
-// }
-// for(auto x : status) {
-//     if(x == 0)
-//         ans = false;
-// }
-// return ans;
+bool ans = true;
+vector<int> status(rooms.size());
+queue<int> q;
+for(int i = 0; i < rooms.size(); i++)
+    status[i] = 0;
+status[0] = 1;
+q.push(0);
+while (!q.empty())
+{
+    int x = q.front();
+    q.pop();
+    for (auto el : rooms[x])
+    {
+        if (status[el] == 1)
+            continue;
+        status[el] = 1;
+        q.push(el);
+    }
+}
+for(auto x : status) {
+    if(x == 0)
+        ans = false;
+}
+return ans;
